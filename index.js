caf43ec2b182fd7d3148d0055d7392a20f772a92
n0sej0b <garrett.l.rogers@gmail.com>
2024-10-29 19:16:50 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // TODO: this file! :)
+const state = {
+    numberBank: [],
+    Odds: [],
+    Evens: [],
+}
+const sectionBank = document.querySelector('#numberBank');
+const bankOutput = sectionBank.querySelector('output');
+const sortOne = document.querySelector('#sortOne');
+const form = document.querySelector('form');
+const inputNum = document.querySelector('#number');
+const sectionOdds = document.querySelector('#odds');
+const oddsOutput = sectionOdds.querySelector('output');
+const sectionEvens = document.querySelector('#evens');
+const evensOutput = sectionEvens.querySelector('output');
+const sortAll = document.querySelector('#sortAll');
+ 
+function showState() {
+    bankOutput.textContent = state.numberBank;
+    oddsOutput.textContent = state.Odds;
+    evensOutput.textContent = state.Evens;
+}
+
+form.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    if(!inputNum.value) {
+        return;
+    } else {
+        state.numberBank.push(inputNum.value);
+    
+    }
+    showState();
+
+})
+
+//Sorting code
+
+sortOne.addEventListener('click' , (event) => {
+    event.preventDefault();
+    if(state.numberBank.length == 0) {
+        return;
+    }
+        while (state.numberBank.length > 0) {
+            const one = state.numberBank.shift();
+            if (one % 2 == 0) {
+                state.Evens.push(one)
+            } else {
+                state.Odds.push(one);
+            }
+
+
+        }
+
+showState();
+})
